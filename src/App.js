36860b4bb17308695a6e5e293a50b98f3fa8bd1e
Andrew - Joseph Blanco <andrewblanco2@outlook.com>
2022-06-15 02:02:47 -0500

    finished Header component nav bar with styled components installed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Counter } from './features/counter/Counter';
+import Header from './components/Header'
+import Home from './components/Home'
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      Hello
+      <Header />
+      <Home />
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,102 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function Header() {
+  return (
+    <Nav>
+        <Logo src="/images/logo.svg" />
+        <NavMenu>
+            <a>
+                <img src="/images/home-icon.svg" alt="" />
+                <span>HOME</span>
+            </a>
+            <a>
+                <img src="/images/search-icon.svg" alt="" />
+                <span>SEARCH</span>
+            </a>
+            <a>
+                <img src="/images/watchlist-icon.svg" alt="" />
+                <span>WATCHLIST</span>
+            </a>
+            <a>
+                <img src="/images/original-icon.svg" alt="" />
+                <span>ORIGINALS</span>
+            </a>
+            <a>
+                <img src="/images/movie-icon.svg" alt="" />
+                <span>MOVIES</span>
+            </a>
+            <a>
+                <img src="/images/series-icon.svg" alt="" />
+                <span>SERIES</span>
+            </a>
+        </NavMenu>
+        <UserImg src="https://scontent-hou1-1.xx.fbcdn.net/v/t1.18169-9/14724370_10211247270806007_120759510992306429_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=174925&_nc_ohc=jP2eaFq6F5cAX-azeL5&_nc_ht=scontent-hou1-1.xx&oh=00_AT-DHCwUhr7VdO9TC8bbe1u3f_9hs4iFNM5cAN-0fzAubw&oe=62CFE3B2"/>
+    </Nav>
+  )
+}
+
+export default Header
+
+const Nav = styled.nav`
+    height: 70px;
+    background: #090b13;
+    display: flex;
+    align-items: center;
+    padding: 0 36px;
+`
+
+const Logo = styled.img`
+    width: 80px;
+
+`
+
+const NavMenu = styled.div`
+    display: flex;
+    flex: 1;
+    margin-left: 25px;
+    align-items: center;
+    a {
+        display: flex;
+        align-items: center;
+        padding: 0 12px;
+        cursor: pointer;
+        img {
+            height: 20px;
+        }
+
+        span {
+            font-size: 13px;
+            letter-spacing: 1.42px;
+            position: relative;
+
+            &:after {
+                content: "";
+                height: 2px;
+                background: white;
+                position: absolute;
+                left: 0;
+                right: 0;
+                bottom: -6px;
+                opacity: 0;
+                transform-origin: left center;
+                transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+                transform: scaleX(0);
+            }
+        }
+
+        &:hover {
+            span:after {
+                transform: scaleX(1);
+                opacity: 1;
+            }
+        }
+    }
+`
+
+const UserImg = styled.img`
+    width: 48px;
+    height: 48px;
+    border-radius: 50%;
+    cursor: pointer;
+`
