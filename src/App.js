13ae7c4071422fe3646838d3a0f236f90c440c6f
ablanco12 <ablanco@appddictionstudio.com>
2022-06-21 16:23:47 -0500

    Added Details component

--- src/App.js
+++ src/App.js
@@ -2,14 +2,31 @@ import React from 'react';
 import { Counter } from './features/counter/Counter';
 import Header from './components/Header'
 import Home from './components/Home'
+import Details from './components/Details'
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Home />
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/details">
+            <Details />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
